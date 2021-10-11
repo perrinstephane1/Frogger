3c366ab5370372c074b5215b21931fb48874413b
WilliamsiNFINITE <91114817+WilliamsiNFINITE@users.noreply.github.com>
2021-10-11 18:24:28 +0200

    modification de la calsse Piste

--- src/Piste.java
+++ src/Piste.java
@@ -1,20 +1,39 @@
+import javafx.scene.Group;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 
+
 public class Piste {
-    protected String terrain;
+    protected Plateau p;
     protected boolean hostile;
     protected double sens;
     protected double vitesse;
     protected double taille_obstacle;
-    protected java.util.ArrayList<Truc_mobile> trucs;
     protected boolean arrivee; // si c'est la dernière ligne c'est FINI
+    protected ImageView imageView;
 
-
-    public Piste(String terrain, boolean hostile, double sens, double vitesse, double taille_obstacle, boolean arrivee){
+    public Piste(int ii, Plateau p, boolean hostile, double sens, double vitesse, double taille_obstacle) {
         this.vitesse = vitesse;
         this.sens = sens;
         this.taille_obstacle = taille_obstacle;
-        //this.scene = scene;
-        //this.height = this.scene.getHeight() / 10.0D;
+        try {
+            Image image = new Image(new FileInputStream("D:\\SOIA_2A\\java\\Frogger\\herbe.png"));
+            this.imageView = new ImageView(image);
+            this.imageView.setX(0);
+            this.imageView.setY(0+ii*p.h_piste);
+            this.imageView.setFitHeight(p.h_piste);
+            this.imageView.setFitWidth(p.l_piste);
+        } catch (FileNotFoundException e) {
+            System.out.println("f***");
+            System.out.println(e);
+        }
+    }
+
+    public ImageView getImageView() {
+        return imageView;
     }
 }
