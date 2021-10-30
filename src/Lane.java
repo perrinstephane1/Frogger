e1bacb8da90e3f51566f074e7d9c627bdd6c51aa
WilliamsiNFINITE <91114817+WilliamsiNFINITE@users.noreply.github.com>
2021-10-30 16:13:29 +0200

    javadocké la classe Lane

--- src/Lane.java
+++ src/Lane.java
@@ -7,7 +7,14 @@ import java.io.FileNotFoundException;
 import java.util.Arrays;
 import java.util.List;
 
-
+/**
+ * This class represents the human-machine interface
+ * @author Williams HOARAU
+ * @author Louis JOGUET
+ * @author Aurelien PARAIRE
+ * @author Stephane PERRIN
+ *
+ */
 public class Lane {
     protected boolean hostile;
     protected double sens;
@@ -24,12 +31,20 @@ public class Lane {
     protected Board board;
 
 
-
+    /**
+     * This method is the constructor
+     * @param ii This int is the number given to the Lane as an ID.
+     * @param p This Board is the one in which the Lane will be put
+     * @param hostile
+     * @param sens This double gives the direction of the Lane (0 : from right to left. 1 : from left to right)//TODO renommer en direction
+     * @param vitesse This double is the speed at which the MovingElement is moving.//TODO renommer en speed
+     * @param densite This double determines the probability of having cars on the Lane//TODO renommer en density
+     * @param type_lane
+     */
     public Lane(int ii, Board p, boolean hostile, double sens, double vitesse, double densite, int type_lane) {
         this.board = p;
         this.vitesse = vitesse;
         this.sens = sens;
-
         this.longueur_bloc = p.l_case;
         this.longueur_lane = p.nb_case * p.l_case;
         this.numero_lane = ii;
@@ -44,7 +59,6 @@ public class Lane {
         else{
             this.taille_obstacle = 3;
         }
-
         if (this.type_lane == 0){ // If it is a road
             this.setImageView("routemieuxJAUNE.png");
         } else if (this.type_lane == 1){  // If it is a river
@@ -53,10 +67,13 @@ public class Lane {
             this.setImageView("debut.png");
         } else if (this.type_lane == 3) {
             this.gridPane.add(p.getChrono(), ii, 0);
-
         }
     }
 
+    /**
+     * This method returns sets the image on the game window.
+     * @param file This String corresponds to the image file to be showed in the game.
+     */
     private void setImageView(String file) {
         try {
             for (int jj = 0; jj < this.board.nb_case; jj++) {
@@ -71,11 +88,16 @@ public class Lane {
         }
     }
 
-
+    /**
+     * This method returns an array with the Lan parameters
+     */
     public List getParametre() {
         return Arrays.asList(this.vitesse, this.sens, this.taille_obstacle, this.longueur_bloc, this.longueur_lane, this.numero_lane, this.densite, this.type_lane);
     }
 
+    /**
+     * This method returns an ImageView.
+     */
     public GridPane getImageView() {
         return this.gridPane;
     }
