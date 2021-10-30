b8e1dd8547e034995aaf87a854ddd45f88b9fc9e
WilliamsiNFINITE <91114817+WilliamsiNFINITE@users.noreply.github.com>
2021-10-30 14:51:09 +0200

    javadocké la classe Log suppression de la méthode auto_down de la classe Log (déjà présente dans la classe mère Truc_mobile)
    javadocké la classe Frog

--- src/Log.java
+++ src/Log.java
@@ -2,10 +2,23 @@ import javafx.animation.TranslateTransition;
 import javafx.scene.Scene;
 import javafx.util.Duration;
 
-
+/**
+ * This class represents the logs on which the frog can be
+ * @author Williams HOARAU
+ * @author Louis JOGUET
+ * @author Aurelien PARAIRE
+ * @author Stephane PERRIN
+ *
+ */
 public class Log extends Truc_mobile{
     protected Piste piste;
 
+    /**
+     * This method is the constructor
+     * @param l_case This int corresponds to the length in pixels of a square on the game windows.
+     * @param piste This Piste corresponds to a lane in the game (river, road or safe lane).
+     * @param scene This Scene will be used in the class Truc_mobile to get its height and width for the game parameters
+     * */ // TODO changer le nom de la classe Truc_mobile
     public Log(Piste piste, Scene scene, int l_case) {
         super(piste, scene, l_case, piste.taille_obstacle);
         this.piste = piste;
@@ -16,11 +29,5 @@ public class Log extends Truc_mobile{
         } else if (piste.taille_obstacle == 3.0){
             this.setImageView("log.png");
         }
-    }
-
-    public void auto_down(int speed) {
-        TranslateTransition trans = new TranslateTransition(Duration.seconds(0.001), this.getImageView());
-        trans.setByY(speed);
-        trans.play();
     }
 }
