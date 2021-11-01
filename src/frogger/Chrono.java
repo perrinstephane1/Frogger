0040246e79079f1bff7921d3693b0be12e91bea2
R3drag0n <r3drag0ngam3r@gmail.com>
2021-11-01 21:54:15 +0100

    Optimisation Board, Car et Chrono

--- src/frogger/Chrono.java
+++ src/frogger/Chrono.java
@@ -9,8 +9,6 @@ package frogger;
  *
  */
 public class Chrono {
-    private final long nanoSecondsPerSecond = 1000000000;
-    private final long nanoSecondPerCentieme = 10000000;
     private long chronoStartTime = 0;
     private long chronoStopTime = 0;
     /** Determines if the chronometer is running or not */
@@ -43,7 +41,7 @@ public class Chrono {
         } else {
             elapsedTime = (chronoStopTime - chronoStartTime);
         }
-        return elapsedTime / nanoSecondsPerSecond;
+        return elapsedTime / 1000000000;
     }
     /**
      * This method returns the elapsed time in second with decimals between the start and the stop of the chronometer
@@ -56,6 +54,6 @@ public class Chrono {
         } else {
             elapsedTime = (chronoStopTime - chronoStartTime);
         }
-        return elapsedTime / nanoSecondPerCentieme;
+        return elapsedTime / 10000000;
     }
 }
