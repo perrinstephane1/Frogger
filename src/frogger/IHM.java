9f8418897f3985acce53628ab24cd0ff8f1cc72b
perrinstephane1 <stephane.perrin@ensta-bretagne.org>
2021-11-03 18:31:40 +0100

    grnouille bien placées

--- src/frogger/IHM.java
+++ src/frogger/IHM.java
@@ -324,13 +324,15 @@ public class IHM extends Application {
             Label explication = new Label("Ce jeu vous permet de jouer avec plusieurs niveaux de difficulté : \n" +
                     "1. Le mode débutant dans lequel les cars et rondins ne vont pas très vite \n" +
                     "2. Le mode intermédiaire dans lequel ils se déplacent plus vite \n" +
-                    "3. Le mode expert dans lequel ils n'iront jamais ausis vite !!  \n");
+                    "3. Le mode expert dans lequel ils n'iront jamais aussi vite !!  \n");
             helpInfo(explication);
         });
         mode.setOnAction(event -> {
             Label explication = new Label("Ce jeu vous permet de jouer de deux façons différentes : \n" +
                     "1. Le mode FINI se termine dès que vous arrivez en haut de l'écran. \n" +
-                    "2. Le mode INFINI ne se termine que lorsque votre grenouille meurt.");
+                    "2. Le mode INFINI ne se termine que lorsque votre grenouille meurt. \n" +
+                    "malheureusement, nous n'avons pas eu le temps d'implémenter une version correcte avec un défilement infini. " +
+                    "Ce bouton ne sert donc à rien");
             helpInfo(explication);
         });
 
@@ -361,7 +363,6 @@ public class IHM extends Application {
         copyright.setWrapText(true);
         copyright.setAlignment(Pos.BOTTOM_CENTER);
 
-        //TODO faire le PLAY Bouton ^^
         play.setOnAction(event -> {
             primaryStage.close();
             boolean test;
@@ -443,9 +444,15 @@ public class IHM extends Application {
      *
      */
     public void joue(boolean joueurs, String name1, String name2, int dif_i){
-
-        this.frog = new Frog((this.nb_case) * this.l_case /2 , (this.nb_case -1)* this.l_case, this.l_case, this.nb_case, "frog8bit.png");
-        this.frog2 = new Frog(0 , (this.nb_case -1)* this.l_case, this.l_case, this.nb_case, "frog8bit2.png");
+        float factor= 0.5F;
+        System.out.println(factor);
+        if (joueurs){
+            System.out.println("on est rentré");
+            factor=2/3F;
+        }
+        System.out.println(factor);
+        this.frog = new Frog((int) ((this.nb_case) * this.l_case*factor), (this.nb_case -1)* this.l_case, this.l_case, this.nb_case, "frog8bit.png");
+        this.frog2 = new Frog((this.nb_case) * this.l_case /3 , (this.nb_case -1)* this.l_case, this.l_case, this.nb_case, "frog8bit2.png");
 
         this.root = new Group();
         this.scene = new Scene(this.root, this.l_case*this.nb_case, this.l_case*(this.nb_case+1));
